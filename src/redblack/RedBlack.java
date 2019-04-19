16f6924b4d13eb4ac6f2dc36faeceb5ac2b673ae
Marwan Saad <marwan10salem@hotmail.com>
2019-04-20 01:05:12 +0200

    Version 4: 4/18 4.5 Insertion Works Fixed stuff made it better

--- src/redblack/RedBlack.java
+++ src/redblack/RedBlack.java
@@ -17,18 +17,18 @@ public class RedBlack {
     public static void main(String[] args) {
         // TODO code application logic here
         Node ROOT = new Node(10);
-        Node.setROOT(ROOT);
 //        ROOT.left = new Node(5);
 //        ROOT.right = new Node(20);
 //        ROOT.left.parent = ROOT;
 //        ROOT.right.parent = ROOT;
 //        ROOT.rightRotate();
 //        Node x= Node.ROOT;
-        ROOT =Node.ROOT;
-        Node.insertIter(ROOT, 20);
-        Node.insertIter(ROOT, 30);
-        Node.insertIter(ROOT, 15);
-        
+        //Node.setROOT(ROOT);
+        //Node.ROOT = null;
+        Node.insert(10);
+        Node.insert(20);
+        Node.insert(30);
+        Node.insert(15);
         Node.Inorder(Node.ROOT);
     }
     
